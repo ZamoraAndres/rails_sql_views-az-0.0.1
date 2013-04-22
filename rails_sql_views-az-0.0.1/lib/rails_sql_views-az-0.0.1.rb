b7133e931cf1c7479c3ba2299e32c47fe0e7b4ae
Andrés <zamoram.andres@gmail.com>
2013-04-21 21:33:25 -0500

    asdfasdf

--- rails_sql_views-az-0.0.1/lib/rails_sql_views-az-0.0.1.rb
+++
@@ -1,9 +0,0 @@
-require "rails_sql_views-az-0.0.1/version"
-
-module RailsSqlViews
-  module Az
-    module 0.0.1
-      # Your code goes here...
-    end
-  end
-end
--- rails_sql_views-az-0.0.1/lib/rails_sql_views-az-0.0.1/version.rb
+++
@@ -1,7 +0,0 @@
-module RailsSqlViews
-  module Az
-    module 0.0.1
-      VERSION = "0.0.1"
-    end
-  end
-end
